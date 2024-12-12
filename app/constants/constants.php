53938d956e6af0290a970356e3eff27590779a53
oswaldohuillca <oswaldohuillca@gmail.com>
2024-12-11 20:29:16 -0500

    feat: added vite server

--- app/constants/constants.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Aprosglobal\Solarwp\constants;
-
-define("SITE_URL", '');
---
+++ templates/home.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Template Name: Home
+ * */
+get_header();
+?>
+<h1 class="text-red-400 mt-40">Home</h1>
+<?php
+get_footer();
